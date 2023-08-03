afe262e50c6a4235ce3ceb357a4ed7a2ce29884a
Nia Stewart <niastewart75@gmail.com>
2023-08-02 21:35:02 -0700

    New Clean RA Script

--- Clean RA Survey.R
+++ Clean RA Survey.R
@@ -4,6 +4,8 @@ setwd("/Users/admin/Desktop/Code it Up/")
 
 #readr package
 library (readr)
+library (tidyr)
+
 
 #import data and recognize as RAdata
 RAdata <-read.csv("RA_survey.csv", header = TRUE, sep = ",")
@@ -33,10 +35,15 @@ names(RAdata)[names(RAdata) == "Q8"] <- "Pre_Debrief_Suspicion"
 names(RAdata)[names(RAdata) == "Q9"] <- "Additional_Considerations"
 
 #Convert columns to uppercase
-
 RAdata$Response_ID <- toupper(RAdata$Response_ID)
 RAdata$Participant_ID <- toupper(RAdata$Participant_ID)
 
 #delete columns
-
 RAdata <- subset(RAdata, select = -c(Start_Date, End_Date, Status, IP_Address, Progress, Duration_Seconds, Finished, Last_Name, First_Name, Email, Reference, Distribution_Channel, User_Language, Longitude, Latitude, Response_ID))
+
+#split Tom Actor column into Tom Actor Name and Tom Actor Race
+RAdata <- RAdata %>% separate(Tom_Actor, c("Tom_Actor_Name", "Tom_Actor_Race"), " ")
+
+#combine Study Id Qs
+data3$Study_ID = paste(data3$Study_ID_Q1, data3$Study_ID_Q2, data3$Study_ID_Q3, data3$Study_ID_Q4, sep="")
+
