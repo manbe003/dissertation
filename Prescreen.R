4eb75cda8d334a3de8158d32e87d889f4de2abec
Paige Griffin <paigegriff99@gmail.com>
2023-09-14 15:25:36 -0700

    Final Clean

---
+++ Prescreen.R
@@ -0,0 +1,113 @@
+#Change the variable names
+names(Prescreen_survey)[names(Prescreen_survey) == "V12"] <- "Medical_student_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V13"] <- "Medical_year_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V14"] <- "Student_age_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V15"] <- "Height_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V16"] <- "Weight_pounds_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V17"] <- "Sex_assigned_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V26"] <- "Previous_telehealth_study_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V33"] <- "SDS_1_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V34"] <- "SDS_2_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V35"] <- "SDS_3_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V36"] <- "SDS_4_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V37"] <- "SDS_5_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V38"] <- "SDS_6_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V39"] <- "SDS_7_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V40"] <- "SDS_8_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V41"] <- "SDS_9_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V42"] <- "SDS_10_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V43"] <- "SDS_11_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V44"] <- "SDS_12_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V45"] <- "SDS_13_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V46"] <- "STAI_trait_1_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V47"] <- "STAI_trait_2_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V48"] <- "STAI_trait_3_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V49"] <- "STAI_trait_4_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V50"] <- "STAI_trait_5_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V51"] <- "STAI_trait_6_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V52"] <- "STAI_trait_7_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V53"] <- "STAI_trait_8_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V54"] <- "STAI_trait_9_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V55"] <- "STAI_trait_10_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V56"] <- "STAI_trait_11_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V57"] <- "STAI_trait_12_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V58"] <- "STAI_trait_13_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V59"] <- "STAI_trait_14_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V60"] <- "STAI_trait_15_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "v61"] <- "STAI_trait_16_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V62"] <- "STAI_trait_17_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V63"] <- "STAI_trait_18_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V64"] <- "STAI_trait_19_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V65"] <- "STAI_trait_20_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V66"] <- "STAI_trait_21_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V67"] <- "STAI_trait_22_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V68"] <- "STAI_trait_23_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V69"] <- "STAI_trait_24_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V70"] <- "STAI_trait_25_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V71"] <- "STAI_trait_26_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V72"] <- "STAI_trait_27_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V73"] <- "STAI_trait_28_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V74"] <- "STAI_trait_29_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V75"] <- "STAI_trait_30_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V76"] <- "STAI_trait_31_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V77"] <- "STAI_trait_32_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V78"] <- "STAI_trait_33_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V79"] <- "STAI_trait_34_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V61"] <- "STAI_trait_16_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V1"] <- "Start_date_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V2"] <- "End_date_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V3"] <- "Response_type_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V4"] <- "Progress_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V5"] <- "Duration_seconds_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V6"] <- "Finished_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V7"] <- "Recorded_date_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V8"] <- "Response_ID_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V10"] <- "User_language_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V11"] <- "Consent_agreement_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V9"] <- "Distribution_channel_pre"
+names(Prescreen_survey)[names(Prescreen_survey) == "V27"] <- "Telehealth_patient_experience_pre"
+
+#Combine Variables into one 
+Personal_ID_pre <- c("V29", "V30", "V31", "V32")
+Prescreen_survey$V29V30V31V32 <- do.call(paste, c(Prescreen_survey[Personal_ID_pre], sep = ""))
+
+Gender_identity_pre <- c("V18", "V19")
+Prescreen_survey$V18V19 <- do.call(paste, c(Prescreen_survey[Gender_identity_pre], sep = ""))
+
+Sexual_orientation_pre <- c("V20", "V21")
+Prescreen_survey$V20V21 <- do.call(paste, c(Prescreen_survey[Sexual_orientation_pre], sep = ""))
+
+Racial_Ethnic_identity_pre <- c("V22", "V23")
+Prescreen_survey$V22V23 <- do.call(paste, c(Prescreen_survey[Racial_Ethnic_identity_pre], sep = ""))
+
+Primary_language_pre <- c("V24", "V25")
+Prescreen_survey$V24V25 <- do.call(paste, c(Prescreen_survey[Primary_language_pre], sep = ""))
+
+
+#Delete individual variables to just show the combined column
+Prescreen_survey <- Prescreen_survey[ , ! colnames(Prescreen_survey) %in% Personal_ID_pre]
+
+Prescreen_survey<- Prescreen_survey[ , ! colnames(Prescreen_survey) %in% Gender_identity_pre]
+
+Prescreen_survey <- Prescreen_survey[ , ! colnames(Prescreen_survey) %in% Sexual_orientation_pre]
+
+Prescreen_survey <- Prescreen_survey[ , ! colnames(Prescreen_survey) %in% Racial_Ethnic_identity_pre]
+
+Prescreen_survey <- Prescreen_survey[ , ! colnames(Prescreen_survey) %in% Primary_language_pre]
+
+# Delete a single column 
+Prescreen_survey <- Prescreen_survey[ , -which(names(Prescreen_survey) %in% c("V28"))]
+
+#Change Name of Combined Variables 
+names(Prescreen_survey)[names(Prescreen_survey) == "V29V30V31V32"] <- "Personal_ID_pre"
+
+names(Prescreen_survey)[names(Prescreen_survey) == "V18V19"] <- "Gender_identity_pre"
+
+names(Prescreen_survey)[names(Prescreen_survey) == "V20V21"] <- "Sexual_orientation_pre"
+
+names(Prescreen_survey)[names(Prescreen_survey) == "V22V23"] <- "Racial_Ethnic_identity_pre"
+
+names(Prescreen_survey)[names(Prescreen_survey) == "V24V25"] <- "Primary_language_pre"
+
+
+#Removing Rows
